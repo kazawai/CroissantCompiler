4ab2b75a31da2428d034805dd3d81e8f4bac968a
Juju <julienladeuze@outlook.fr>
2024-04-13 18:01:58 +0200

    fixed print issue with list and sequence

--- modules/types/print.py
+++ modules/types/print.py
@@ -2,17 +2,25 @@ from enum import Enum
 
 from modules.utils.wrapper import Wrapper
 
-bool_t = {True : "vrai", False : "faux"}
+def display_bool(b):
+    bool_t = {True : "vrai", False : "faux"}
+    return bool_t[b]
+
+def display_list(l):
+    return "[%s]" % ', '.join(map(str, l))
+
+display_items = {list : display_list, bool : display_bool}
 
 def display(args):
-    if type(args[1]) != list:
-        if type(args[1]) == bool:
-            print(bool_t[args[1]])
-        else:
-            print(args[1])
-    else:
-        items = list(map(lambda x : bool_t[x] if type(x) == bool else x, args[1]))
-        print(" ".join(map(str,items)))
+    if type(args[1]) in display_items:
+        print(display_items[type(args[1])](args[1]))
+    print(args[1])
+
+
+def display_seq(args):
+    items = args[1:]
+    items = list(map(lambda x : display_items[type(x)](x) if type(x) in display_items else x, items))
+    print(" ".join(map(str,items)))
 
 class Print(Enum):
     """
@@ -20,4 +28,5 @@ class Print(Enum):
         used to define how print operations are defined
     """
 
+    PRINT_SEQ = Wrapper(display_seq)
     PRINT = Wrapper(display)
