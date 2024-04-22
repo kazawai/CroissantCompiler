15551656f2104a78315e47c70dc1934a2e79cae1
kazawai <bazinmaxime0@gmail.com>
2024-04-22 21:32:41 +0200

    User input is already done
    Removed todo comment

--- modules/utils/global_var.py
+++ modules/utils/global_var.py
@@ -2,11 +2,11 @@ def init():
     """
     All the needed variables needed "system-wide"
     """
-    global context #variables
-    global nested_counter #block
-    global line_counter #number of lines passed
-    global debug #flag for debug see -h
-    global input #the user input TODO i guess ?
+    global context  # variables
+    global nested_counter  # block
+    global line_counter  # number of lines passed
+    global debug  # flag for debug see -h
+    global input  # the user input
     context = {}
     nested_counter = 0
     line_counter = 1
