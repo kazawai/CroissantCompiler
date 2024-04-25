226ad207f08d1a5da9d87b46bd0e258fe291bd9a
kazawai <bazinmaxime0@gmail.com>
2024-04-25 11:45:44 +0200

    Added size of string

--- modules/types/texte.py
+++ modules/types/texte.py
@@ -2,9 +2,12 @@ from enum import Enum
 
 from modules.utils.wrapper import Wrapper
 
+
 class StringExpression(Enum):
     """
     String Expr:
         used to define how operations are defined on string values
     """
-    STRING_ATOMIC_VALUE = Wrapper(lambda args : args[1:len(args) - 1])
+
+    STRING_ATOMIC_VALUE = Wrapper(lambda args: args[1 : len(args) - 1])
+    SIZE_TEXT = Wrapper(lambda args: len(args[1]))
