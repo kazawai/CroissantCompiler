8ceef1a260b0c176b0bf57c5aa5e9c597e4ed13e
Juju <julienladeuze@outlook.fr>
2024-04-30 14:47:18 +0200

    fixed ('a' + 'b')[0]

--- modules/types/texte.py
+++ modules/types/texte.py
@@ -19,3 +19,4 @@ class StringExpression(Enum):
     STRING_ATOMIC_VALUE = Wrapper(lambda args: args[1 : len(args) - 1])
     SIZE_STRING = Wrapper(lambda args: len(args[1]))
     INDEX_STRING = Wrapper(index_string)
+    CONCAT_STRING = Wrapper(lambda args: args[0] + args[1])
